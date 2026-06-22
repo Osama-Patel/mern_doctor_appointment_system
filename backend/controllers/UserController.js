879c6afc0d02e313e464b4c02c473eb5682f399e
Osama Patel <usamapatel4546@gmail.com>
2026-06-22 22:50:54 +0530

    modified files

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,7 +7,6 @@ const User = require('../models/Users');
 const Doctor = require('../models/Doctors');
 
 dotenv.config()
-//User Routes
 router.get('/',async (req,res)=>{
     const data=await User.find()
     res.send(data)
